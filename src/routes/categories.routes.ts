f25e7386db6d62b44b8b407c7ae822b4a8094776
Remo de Oliveira Gresta <remoogg@gmail.com>
2021-07-27 12:40:52 -0300

    insert specification

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,26 +1,16 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../repositories/CategoriesRepository";
-import { CreateCategoryService } from "../services/CreateCategoryService";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoryRoute = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoryRoute.post("/", (request, response) => {
-    const { name, description } = request.body;
-
-    const createCategoryService = new CreateCategoryService(
-        categoriesRepository
-    );
-
-    createCategoryService.execute({ name, description });
-
-    return response.status(201).send();
+    return createCategoryController.handle(request, response);
 });
 
 categoryRoute.get("/", (request, response) => {
-    const list = categoriesRepository.list();
-    return response.json(list);
+    return listCategoriesController.handle(request, response);
 });
 
 export { categoryRoute };
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 
 import { categoryRoute } from "./routes/categories.routes";
+import { specificationsRoutes } from "./routes/specifications.routes";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/categories", categoryRoute);
+app.use("/specifications", specificationsRoutes);
 
 app.listen(3333, () => console.log("Running on port 3333"));
