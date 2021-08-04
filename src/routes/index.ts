9f44edaf27fb0b8c8c543a263651c316cbc343eb
Remo de Oliveira Gresta <remoogg@gmail.com>
2021-08-04 02:38:28 -0300

    add users

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import { categoryRoute } from "./categories.routes";
 import { specificationsRoutes } from "./specifications.routes";
+import { usersRoutes } from "./users.routes";
 
 const router = Router();
 
 router.use("/categories", categoryRoute);
 router.use("/specifications", specificationsRoutes);
+router.use("/users", usersRoutes);
 
 export { router };
