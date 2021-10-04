4cab55d2f7dbbe4e494249e6a446f1ecaef3d43b
Remo de Oliveira Gresta <remoogg@gmail.com>
2021-10-03 22:45:06 -0300

    add

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,8 +1,8 @@
 import "reflect-metadata";
+import upload from "config/upload";
 import express, { Request, Response, NextFunction } from "express";
 import swaggerUi from "swagger-ui-express";
 
-import upload from "@config/upload";
 import createConnection from "@shared/infra/typeorm";
 
 import swaggerFile from "../../../swagger.json";
